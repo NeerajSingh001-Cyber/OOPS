0de7b0044454c525f7d957ceb66fc10a736cb982
Neeraj Singh <neeraj.singh.rawat1530@gmail.com>
2025-07-03 15:20:53 +0530

    Add files via upload

---
+++ firstprogram.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n;
+   int m;
+   cin>>n;
+   cin>>m;
+   cout<<n*m;
+   cout<<"\nheloo";
+}
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   int j;
+   cin>>j;
+   
+   for(int i=1;i<=j;i++)
+   {
+      if(j%i!=0)
+      {
+         cout<<i<<" ";
+      }
+   }
+   return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   int m;
+   cin>>m;
+   for (int i = 0; i < m; i++)
+   {
+      if(i%2==0)
+      {
+         cout<<"even"<<endl<<i<<" ";
+      }
+      else
+      {
+         cout<<"odd"<<endl<<i<<" ";
+      }
+   }
+   
+}
