94798b47699cead3fd29006794db095f33fa9a0c
Neeraj Singh <neeraj.singh.rawat1530@gmail.com>
2025-07-09 14:26:59 +0530

    grade calculator

--- firstprogram.cpp
+++ firstprogram.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 int main()
@@ -49,3 +50,57 @@ int main()
    }
    
 }
+=======
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    int n;
+//    int m;
+//    cin>>n;
+//    cin>>m;
+//    cout<<n*m;
+//    cout<<"\nheloo";
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    int j;
+//    cin>>j;
+   
+//    for(int i=1;i<=j;i++)
+//    {
+//       if(j%i!=0)
+//       {
+//          cout<<i<<" ";
+//       }
+//    }
+//    return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    int m;
+//    cin>>m;
+//    for (int i = 0; i < m; i++)
+//    {
+//       if(i%2==0)
+//       {
+//          cout<<"even"<<endl<<i<<" ";
+//       }
+//       else
+//       {
+//          cout<<"odd"<<endl<<i<<" ";
+//       }
+//    }
+   
+// }
+
+>>>>>>> 4dd706f (Merged remote master with local)
